0b9a2bf592f872ad60b2d71762c0106613c25ad0
Lukas Ehlers <ehlers@outson.com>
2023-05-19 21:22:47 +0200

    adding infinite carusel opcodes

--- src/components/Opcodes/opcodes.tsx
+++ src/components/Opcodes/opcodes.tsx
@@ -1,29 +1,80 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./opcodes.css";
 import opcodes from "./opcodes.json";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import { IconContext } from "react-icons";
+import { IoSearchSharp } from "react-icons/io5";
+
+interface Opcode {
+  uint8: string;
+  mnemonic: string;
+  description: string;
+}
 
 function Opcodes() {
-  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [searchText, setSearchText] = useState("");
+  const carousel = useRef<AliceCarousel | null>(null);
 
+  const filteredOpcodes: Opcode[] = opcodes.filter(
+    (opcode: Opcode) =>
+      opcode.uint8.toLowerCase().includes(searchText.toLowerCase()) ||
+      opcode.mnemonic.toLowerCase().includes(searchText.toLowerCase()) ||
+      opcode.description.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  const handleOnDragStart = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) =>
+    e.preventDefault();
+
+  const items = filteredOpcodes.map((opcode: Opcode) => (
+    <div className="singleopcontainer">
+      <div
+        className="singleopcode"
+        key={opcode.uint8}
+        onDragStart={handleOnDragStart}
+      >
+        <div className="title">
+          <span className="uint8">{opcode.uint8}</span>
+          <span className="name">{" " + opcode.mnemonic}</span>
+        </div>
+        <span className="description">{opcode.description}</span>
+      </div>
+    </div>
+  ));
+
+  // Slide to the middle index after the component has mounted
   useEffect(() => {
-    const container = containerRef.current;
-    if (container) {
-      container.scrollLeft = container.scrollWidth / 2;
+    if (carousel.current) {
+      carousel.current.slideTo(Math.floor(filteredOpcodes.length / 2));
     }
-  }, []);
+  }, [filteredOpcodes.length]);
 
   return (
     <div className="Opcodes">
-      <div className="opcodescontainer" ref={containerRef}>
-        {opcodes.map((opcode) => (
-          <div className="singleopcode" key={opcode.uint8}>
-            <div className="title">
-              <span className="uint8">{opcode.uint8}</span>
-              <span className="name">{" " + opcode.mnemonic}</span>
-            </div>
-            <span className="description">{opcode.description}</span>
+      <div className="opcodescontainer">
+        <AliceCarousel
+          items={items}
+          ref={carousel as any}
+          infinite={true}
+          autoWidth={true}
+          disableButtonsControls
+          disableDotsControls
+          mouseTracking
+
+        />
+      </div>
+      <div className="searchContainer">
+        <IconContext.Provider value={{ className: "icon-search" }}>
+          <div className="searchIcon_container">
+            <IoSearchSharp />
           </div>
-        ))}
+        </IconContext.Provider>
+        <input
+          type="text"
+          className="searchOp"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
       </div>
     </div>
   );
